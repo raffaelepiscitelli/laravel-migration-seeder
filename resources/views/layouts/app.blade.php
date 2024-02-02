925d3387f92f6883f3c3b9a8ab97274175fa5f8c
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-02-02 15:33:45 +0100

    Modifica iniziale template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>@yield('title', 'Laravel')</title>
     @vite('resources/js/app.js')
+    @yield('head-content')
 </head>
 <body>
-    <h1>Hello, world!</h1>
+    @include('partials.header')
+
+    <main>
+        @yield('main-content')
+    </main>
+    @include('partials.footer')
 </body>
 </html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('main-content')
+    <h1>Hello </h1>
+@endsection
