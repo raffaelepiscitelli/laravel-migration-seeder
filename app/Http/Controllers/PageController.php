da056c4ae3c98491f64a32cf3ecc0a08eb8114c9
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-02-08 22:13:55 +0100

    Esercizio completo

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+   public function index() {
+        $trains = Train::all();
+        return view('train.index', ['trains' => $trains]);
+    }
+}
